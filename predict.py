55bada9d09ca903d5145a2a511f30558449d4f88
yoh-gilang <93894711+Yohgilang@users.noreply.github.com>
2024-06-01 16:31:33 +0700

    Delete predict.py

--- predict.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# import csv
-
-model = YOLO('D:\KULIAH\_Tugas Akhir\Project\Code\deteksi_penyakit _tanaman_terung_TA\eggplant_desease.pt', task='detect') 
-
-results = model(source= "D:\\KULIAH\\_Tugas Akhir\\Project\\Code\\deteksi_penyakit _tanaman_terung_TA\\test", show=False, conf=0.3, save=True)
